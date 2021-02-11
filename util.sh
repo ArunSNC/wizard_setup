94d5de07bfb85d87e9d02746df1a612179298e27
bunny9971 <arunkumarsnc99@outlook.com>
2021-02-11 17:07:09 +0530

    getty service

--- util.sh
+++ util.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install git python3-dev gcc
+apt-get install git python3-dev gcc -y
 
 echo "Enter username : "
 read name
@@ -8,7 +8,9 @@ useradd -m $name
 echo "Enter password for $name : "
 passwd $name
 
-sed -i "34s/.*/--autologin wizard_user/" /lib/systemd/system/serial-getty@.service
-sed -i "s:wizard_user:$name" /lib/systemd/system/serial-getty@.service
+sed -i "34i ExecStart=-/sbin/agetty --autologin $name --keep-baud 115200,38400,9600 %I $TERM:" /lib/systemd/system/serial-getty@.service
+# sed -i "s:wizard_user:$name" /lib/systemd/system/serial-getty@.service
+# sed -i "s:TERM:$TERM" /lib/systemd/system/serial-getty@.service
+sed "35d" /lib/systemd/system/serial-getty@.service
 
-sed -i '11i11 Restart=always' /etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
+sed -i '11i Restart=always' /etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
